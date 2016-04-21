65e624e3821b198b4eae45822266119130f8db9f
zhangrenlong <renlong.zhang@ramy-inc.com>
2016-04-21 10:04:08 -0700

    Initial Whole Project By Renlong Zhang

---
+++ Team-IOS-Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Team-IOS-Client
+//
+//  Created by 张仁龙 on 4/21/16.
+//  Copyright © 2016 张仁龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

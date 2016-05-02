c5fe784e70816737bfde8cadd8940901adc348e0
Fangjia Li <hollandlinus@gmail.com>
2016-05-02 00:20:41 -0700

    detailed screen

--- Team-IOS-Client/DetailedResultViewController.swift
+++ Team-IOS-Client/DetailedResultViewController.swift
@@ -11,45 +11,32 @@ import UIKit
 class DetailedResultViewController: UIViewController, UIScrollViewDelegate {
     var detailedResult:NSDictionary=[:]
 
-    @IBOutlet weak var coverImg: UIImageView!
-    @IBOutlet weak var placeName: UILabel!
-    @IBOutlet weak var landlordName: UILabel!
+    @IBOutlet weak var scrollImg: UIScrollView!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var brief: UILabel!
     @IBOutlet weak var headIcon: UIImageView!
     @IBOutlet weak var rooms: UILabel!
     @IBOutlet weak var price: UILabel!
-    @IBOutlet weak var dscrp: UILabel!
+    @IBOutlet weak var dscrption: UILabel!
     @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var email: UILabel!
-    @IBOutlet weak var scrollableImg: UIScrollView!
-    var colors:[UIColor] = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor()]
-
-    var frame: CGRect = CGRectMake(0, 734, 400, 266)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(detailedResult)
+        let tmp_street = detailedResult["street"] as! String
+        let tmp_city = detailedResult["city"]as! String
+        let tmp_state = detailedResult["state"]as! String
+        let tmp_zipcode = detailedResult["zipcode"]as! String
+        let tmp_address = tmp_street + ", " + tmp_city + " " + tmp_state + " " + tmp_zipcode
+        address.text = tmp_address
+        brief.text = detailedResult["brief"] as? String
+        rooms.text = detailedResult["rooms"] as? String
+        price.text = detailedResult["price"] as? String
+        dscrption.text = detailedResult["description"] as? String
+        phone.text = detailedResult["phone"] as? String
+        email.text = detailedResult["email"] as? String
         
-        self.scrollableImg.frame = CGRectMake(0, 734, self.view.frame.width, self.view.frame.height)
-        let scrollViewWidth:CGFloat = self.scrollableImg.frame.width
-        let scrollViewHeight:CGFloat = self.scrollableImg.frame.height
-        
-        let imgOne = UIImageView(frame: CGRectMake(0, 734,scrollViewWidth, scrollViewHeight))
-        imgOne.image = UIImage(named: "Slide 1")
-        let imgTwo = UIImageView(frame: CGRectMake(scrollViewWidth, 734,scrollViewWidth, scrollViewHeight))
-        imgTwo.image = UIImage(named: "Slide 2")
-        let imgThree = UIImageView(frame: CGRectMake(scrollViewWidth*2, 734,scrollViewWidth, scrollViewHeight))
-        imgThree.image = UIImage(named: "Slide 3")
-        let imgFour = UIImageView(frame: CGRectMake(scrollViewWidth*3, 734,scrollViewWidth, scrollViewHeight))
-        imgFour.image = UIImage(named: "Slide 4")
-        
-        self.scrollableImg.addSubview(imgOne)
-        self.scrollableImg.addSubview(imgTwo)
-        self.scrollableImg.addSubview(imgThree)
-        self.scrollableImg.addSubview(imgFour)
-        
-        self.scrollableImg.contentSize = CGSizeMake(self.scrollableImg.frame.width * 4, self.scrollableImg.frame.height)
-        self.scrollableImg.delegate = self
-     
     }
 
 }

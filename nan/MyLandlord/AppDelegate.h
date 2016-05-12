39777b76268ae31f2d2fc274f597e5d041e5c6f9
NanSJ <nan.ding@gmail.com>
2016-05-12 14:27:01 +0800

    Add files via upload
    add edit view controller

---
+++ nan/MyLandlord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLandlord
+//
+//  Created by myself on 16/5/10.
+//  Copyright (c) 2016 mylandlord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nan/MyLandlord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyLandlord
+//
+//  Created by 长江日报 on 16/5/10.
+//  Copyright (c) 2016年 mylandlord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
